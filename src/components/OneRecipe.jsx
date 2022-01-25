2fb50109e7df7c97ca505ebdb896c2bcabbd3d45
Milica Maslarevic <milicamaslarevic@gmail.com>
2022-01-25 19:18:58 +0100

    Prosleđeni atributi pomoću propertija

--- src/components/OneRecipe.jsx
+++ src/components/OneRecipe.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import {AiOutlineLike} from "react-icons/ai"
 import {AiOutlineHeart} from "react-icons/ai"
 
-function OneRecipe() {
+function OneRecipe(props) {
   return (
   <div className="card"> 
     <img className="card-img" src="https:/picsum.photos/200" alt="Photo"/>
     <div className="card-body">
-        <h3 className="card-title">Recipe</h3>
-        <p className="card-text">This is the recipe</p>
+        <h3 className="card-title">{props.recipe.title}</h3>
+        <p className="card-text">{props.recipe.description}</p>
     </div> 
 
     <AiOutlineLike className='like'/>
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -3,12 +3,45 @@ import OneRecipe from './OneRecipe';
 
 
 const Recipes = () => {
+    const recipes = [
+        {
+          id: 1,
+          title: "Triple Chocolate Chunk Cookies",
+          description:
+            "Large or small, these triple chocolate cookies are crispy on the outside and chewy on the inside.",
+          amount: 0,
+        },
+        {
+          id: 2,
+          title: "One Bowl Chocolate Cake ",
+          description:
+            "This is a rich and moist chocolate cake. It only takes a few minutes to prepare the batter. Frost with your favorite chocolate frosting.",
+          amount: 0,
+        },
+        {
+          id: 3,
+          title: "Ice Cream",
+          description:
+            "Make homemade ice cream in exactly the flavor you crave. 31 flavors? We've got way more including peach, pumpkin, even coconut-avocado.",
+          amount: 0,
+        },
+        {
+            id: 4,
+            title: "Cheesecake",
+            description:
+              "Baked or no-bake, dark chocolate or white chocolate, you'll find the cheesecake you're looking for right here. A chocolate lover's delight! Top with cherry pie filling or sweetened strawberries.",
+            amount: 0,
+
+
+        },
+      ];
+      
   return (
     <div className='all_recipes'>
-        <OneRecipe/>
-        <OneRecipe/>
-        <OneRecipe/>
-        <OneRecipe/>
+        <OneRecipe recipe={recipes[0]} />
+        <OneRecipe recipe={recipes[1]}/>
+        <OneRecipe recipe={recipes[2]}/>
+        <OneRecipe recipe={recipes[3]}/>
     </div>);
 };
 
