b6bd84c4d474d176917da042caaeb4e5c3c7ead2
Milica Maslarevic <milicamaslarevic@gmail.com>
2022-01-26 12:39:00 +0100

    Izmene u stilizovanju

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -4,7 +4,7 @@ import OneRecipe from './OneRecipe';
 const Favourites = ({recipes}) => {
   return <div>
 
-      <p>Omiljeni</p>
+      <h1 className='haFav'>Favourite recipes</h1>
      { recipes.map((rec)=>(<OneRecipe recipe={rec} key={rec.id} inFav={0}/>))}
   </div>;
 };
